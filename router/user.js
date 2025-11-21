5db50a5b2b8c635f3fc4feb72048a49265618f27
Stanley Pageot <stanley.pageot@gmail.com>
2025-11-21 10:37:17 -0500

    add user register and login with error handling

--- router/user.js
+++ router/user.js
@@ -1,25 +1,85 @@
 import express from "express";
 import User from "../schema/userSchema.js";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 const router = express.Router();
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findOne({ _id: req.params.id });
-  console.log(user);
-  return res.send(user);
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return res.status(200).send(user);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
 });
 
+//TODO: add utility functions for error handling amd hashing
 router.post("/register", async (req, res) => {
-  const user = await User.create(req.body);
-  return res.status(201).json(user);
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    console.log(user);
+    if (user) {
+      throw new Error("User already exists!");
+    }
+    const userPassword = req.body.password;
+    if (!userPassword) {
+      throw new Error("User Password Required!");
+    }
+    if (userPassword.length < 8) {
+      throw new Error("Password must be 8 characters or more");
+    }
+    bcrypt.hash(userPassword, saltRounds, async (err, hash) => {
+      if (err) {
+        throw new Error("Error Hashing Password");
+      }
+      const userDetails = {
+        ...req.body,
+        password: hash,
+      };
+      const user = await User.create(userDetails);
+      return res.status(201).json(user);
+    });
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { password, username } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      throw new Error("Username not found! Register");
+    }
+    const match = bcrypt.compare(password, user.password);
+
+    if (!match) {
+      throw new Error("Password does not match!");
+    }
+    return res.status(200).send(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 router.put("/:id", async (req, res) => {
-  const user = await User.findOneAndUpdate(
-    { _id: req.params.id },
-    { ...req.body }
-  );
-  return res.send(user);
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      { ...req.body }
+    );
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return res.status(204).send(user);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
@@ -27,11 +87,11 @@ router.delete("/:id", async (req, res) => {
     const user = await User.findByIdAndDelete(req.params.id);
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      throw new Error("User not found");
     }
-    return res.send("User Deleted!!");
+    return res.status(204).send("User Deleted!!");
   } catch (err) {
-    res.json({ error: err.message });
+    return res.status(500).send({ error: err.message });
   }
 });
 
--- schema/userSchema.js
+++ schema/userSchema.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
+  username: String,
   firstName: String,
   lastName: String,
+  password: String,
   email: String,
   phoneNumber: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default mongoose.model("User", userSchema);
