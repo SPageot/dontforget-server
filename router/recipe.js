de9d59635d8f2761334a470463008a4067a7016a
Stanley Pageot <stanley.pageot@gmail.com>
2025-12-16 21:04:10 -0500

    get response from model

--- router/recipe.js
+++ router/recipe.js
@@ -9,11 +9,10 @@ router.post("/", async (req, res) => {
     const chatInput = req.body.chatInput
     const response = await axios.post(N8N_URL, {
         
-            message: `respond only with a recipe for ${chatInput} in an array of objects`
+            message: `respond only with a recipe for ${chatInput} in an array of objects without instructions`
         
     });
     const recipeDetails = JSON.parse(response.data[0].output.match(/\{[\s\S]*\}|\[[\s\S]*\]/))
-    console.log(recipeDetails)
     return res.status(200).json(recipeDetails)
 })
 
